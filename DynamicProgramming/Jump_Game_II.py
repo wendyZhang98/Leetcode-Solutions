ed82b8a0db38398453a9ec9e505a682aebe53ed9
WenxinZhang <wd1030044707@icloud.com>
2021-10-15 22:45:30 -0400

    Update Jump_Game_II.py

--- DynamicProgramming/Jump_Game_II.py
+++ DynamicProgramming/Jump_Game_II.py
@@ -18,16 +18,28 @@
 # 假设你总是可以到达数组的最后一个位置。
 
 
-# ？？？
-# class Solution:
-#     def jump(self, nums):
-#         end = mx = steps = 0
-#         for i, num in enumerate(nums[:-1]):
-#             mx = max(mx, i + num)
-#             if i == end:
-#                 end = mx
-#                 steps += 1
-#         return steps
-#
-#
-# print(Solution().canJump(nums=[2, 3, 1, 3, 4]))
+
+### Solution:
+# 具体的实现中，我们维护当前能够到达的最大下标位置，记为边界。
+# 我们从左到右遍历数组，到达边界时，更新边界并将跳跃次数增加 1。
+
+# 在遍历数组时，我们不访问最后一个元素，
+# 这是因为在访问最后一个元素之前，我们的边界一定大于等于最后一个位置，
+# 否则就无法跳到最后一个位置了。
+# 如果访问最后一个元素，在边界正好为最后一个位置的情况下，
+# 我们会增加一次「不必要的跳跃次数」，因此我们不必访问最后一个元素。
+
+# https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+class Solution:
+    def jump(self, nums):
+        n = len(nums)
+        maxPos, end, step = 0, 0, 0
+        for i in range(n - 1):
+            if maxPos >= i:
+                maxPos = max(maxPos, i + nums[i])
+                if i == end:
+                    end = maxPos
+                    step += 1
+        return step
+
+# print(Solution().jump(nums=[2, 3, 1, 3, 4]))

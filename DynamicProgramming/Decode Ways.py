cec793aaf9e8f4e14d1a0da26c6e45880568b90f
WenxinZhang <wd1030044707@icloud.com>
2021-10-16 11:33:58 -0400

    Update Decode Ways.py

--- DynamicProgramming/Decode Ways.py
+++ DynamicProgramming/Decode Ways.py
@@ -70,4 +70,18 @@ class Solution:
             if i > 1 and s[i-2] != '0' and (int(s[i - 2]) * 10 + int(s[i - 1]) <= 26):
                 dp[i] += dp[i-2]
         return dp[n]
+  
+### 优化空间
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        a, b, c = 0, 1, 0
+        for i in range(1, n + 1):
+            c = 0
+            if s[i - 1] != '0':
+                c += b
+            if i > 1 and s[i - 2] != '0' and (int(s[i - 2]) * 10 + int(s[i - 1]) <= 26):
+                c += a
+            a, b = b, c
+        return c
 

dbcdf7455db8e5554d4b50299dbe34b57f6a7dde
Wendy ZHANG <wendyzhang@Wendys-MacBook-Air.local>
2021-10-16 23:00:13 -0400

    Longest Palindromic Substring

---
+++ DynamicProgramming/Longest Palindromic Substring.py
@@ -0,0 +1,69 @@
+### Link:
+# https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0005.Longest%20Palindromic%20Substring/README.md
+
+
+### Description:
+# 给你一个字符串 s，找到 s 中最长的回文子串
+
+
+### Example:
+# 输入：s = "babad"
+# 输出："bab"
+# 解释："aba" 同样是符合题意的答案。
+
+# 输入：s = "cbbd"
+# 输出："bb"
+
+# 输入：s = "a"
+# 输出："a"
+
+# 输入：s = "ac"
+# 输出："a"
+
+
+### Solution:
+# 动态规划法
+# 设 dp[i][j] 表示字符串 s[i..j] 是否为回文串
+# 当 j - i < 2，即字符串长度为 2 时，只要 s[i] == s[j]，dp[i][j] 就为 true
+# 当 j - i >= 2，dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j]
+
+
+# https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
+
+class Solution:
+    def longestPalindrome(self, s):
+        n = len(s)
+        if n < 2:
+            return s
+        max_len = 1
+        begin = 0
+        # dp[i][j] 表示 s[i..j] 是否是回文串
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        # 递推开始
+
+        # 先枚举子串长度
+        for L in range(2, n + 1):
+            # 枚举左边界，左边界的上限设置可以宽松一些
+            for i in range(n):
+                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                j = L + i - 1
+                # 如果右边界越界，就可以退出当前循环
+                if j >= n:
+                    break
+
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+
+                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+                    
+        return s[begin:begin + max_len]

ec5ff10e39675ce085e825186f4659dee06a0c02
WenxinZhang <wd1030044707@icloud.com>
2021-11-08 23:25:34 -0500

    Update Majority Element.py

--- Array/Majority Element.py
+++ Array/Majority Element.py
@@ -26,9 +26,15 @@
 ### Solution:
 # 摩尔投票法
 # 时间复杂度 O(n)，空间复杂度 O(1)
+ 
+# 摩尔投票法（Boyer–Moore majority vote algorithm），也被称作「多数投票法」
+# 算法解决的问题是：如何在任意多的候选人中（选票无序），选出获得票数最多的那个。
+# 算法可以分为两个阶段：
+
+# 对抗阶段：分属两个候选人的票数进行两两对抗抵消
+# 计数阶段：计算对抗结果中最后留下的候选人票数是否有效
 
 
-###
 class Solution:
     def majorityElement(self, nums):
         cnt = major = 0
@@ -39,3 +45,5 @@ class Solution:
             else:
                 cnt += (1 if major == num else -1)
         return major
+
+print(Solution().majorityElement(nums=[4, 4, 4, 3, 3, 4]))

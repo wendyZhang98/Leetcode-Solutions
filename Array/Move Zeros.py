84f3858d5d48663030dd9781d117d5bfe4f243a3
WenxinZhang <wd1030044707@icloud.com>
2021-11-09 10:09:46 -0500

    Update Move Zeros.py

--- Array/Move Zeros.py
+++ Array/Move Zeros.py
@@ -14,22 +14,28 @@
 # 尽量减少操作次数。
 
 
-###
+### Solution 
+# 使用双指针，左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
+# 右指针不断向右移动，每次右指针指向非零数，则将左右指针对应的数交换，同时左指针右移。
+
+# 注意到以下性质：
+
+# 左指针左边均为非零数；
+# 右指针左边直到左指针处均为零。
+# 因此每次交换，都是将左指针的零与右指针的非零数交换，且非零数的相对顺序并未改变。
+
+
 class Solution:
     def moveZeroes(self, nums):
-        """
-        Do not return anything,
-        modify nums in-place instead.
-        """
-        if not nums:
-            return
         n = len(nums)
-        zero_count = 0
-        for i in range(n):
-            if nums[i] == 0:
-                zero_count += 1
-            else:
-                nums[i - zero_count] = nums[i]
-        while zero_count > 0:
-            nums[n - zero_count] = 0
-            zero_count -= 1
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+        
+        return nums
+   
+
+print(Solution().moveZeroes(nums=[1,0,0,5,7,0,8]))

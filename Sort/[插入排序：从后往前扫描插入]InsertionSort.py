478515ba9a29518037ccf2cf82632e2623cdd56b
WenxinZhang <wd1030044707@icloud.com>
2022-01-09 13:58:53 -0500

    Update [插入排序：从后往前扫描插入]InsertionSort.py

--- Sort/[插入排序：从后往前扫描插入]InsertionSort.py
+++ Sort/[插入排序：从后往前扫描插入]InsertionSort.py
@@ -1,9 +1,18 @@
 ### Link:
 # https://www.runoob.com/python3/python-insertion-sort.html
+# https://www.youtube.com/watch?v=uvxHAH3Wq2I
 
 
 ### 插入排序（英语：Insertion Sort）是一种简单直观的排序算法
 # 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入
+
+
+### 算法步骤：
+# 从第二个元素（第一个需要被排序的元素）开始，从后向前扫描之前的元素序列
+# 如果当前扫描的元素大于新元素，将扫描元素移动到下一位
+# 重复步骤2，直到找到一个小于等于新元素的位置
+# 将新元素插入到该位置
+# 对于之后的元素重复步骤1-4
 
 
 def insertionSort(arr):

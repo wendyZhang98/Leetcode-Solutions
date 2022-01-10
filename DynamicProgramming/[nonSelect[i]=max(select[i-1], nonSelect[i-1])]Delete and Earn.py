1212abe649bd335cbdb75f064c559c73df0729d9
WenxinZhang <wd1030044707@icloud.com>
2022-01-10 09:51:23 -0500

    Update and rename Delete and Earn.py to [nonSelect[i]=max(select[i-1], nonSelect[i-1])]Delete and Earn.py

--- DynamicProgramming/[nonSelect[i]=max(select[i-1], nonSelect[i-1])]Delete and Earn.py
+++ DynamicProgramming/[nonSelect[i]=max(select[i-1], nonSelect[i-1])]Delete and Earn.py
@@ -2,12 +2,14 @@
 # https://github.com/doocs/leetcode/blob/main/solution/0700-0799/0740.Delete%20and%20Earn/README_EN.md
 
 
+
 ### Description:
 # 给定一个整数数组 nums，你可以对它进行一些操作
 # 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数
 # 之后，你必须删除每个等于 nums[i]-1 或 nums[i]+1 的元素
 # 开始你拥有 0 个点数
 # 返回你能通过这些操作获得的最大点数
+
 
 
 ### Example:
@@ -25,6 +27,7 @@
 # 总共获得 9 个点数。
 
 
+
 ### Solution：
 # 核心思路: 一个数字要么不选，要么全选
 # 首先计算出每个数字的总和 sums，并维护两个 dp 数组：select 和 nonSelect
@@ -38,23 +41,23 @@
 # 如果不选 i，那么 i-1 选不选都可以，因此我们选择其中较大的选法
 # select[i] = nonSelect[i-1] + sums[i];
 # nonSelect[i] = Math.max(select[i-1], nonSelect[i-1]) = Math.max(nonSelect[i-2] + sums[i-1], nonSelect[i-1])
-
+# 不断迭代更新 nonSelect[i]
 
 class Solution:
     def deleteAndEarn(self, nums):
         mx = float('-inf')
         for num in nums:
             mx = max(mx, num)
-        total = [0] * (mx + 1)
+        total = [0] * (mx + 1) # sum_lst
         for num in nums:
-            total[num] += num
+            total[num] += num  # sum_lst
         first = total[0]
         second = max(total[0], total[1])
         for i in range(2, mx + 1):
-            cur = max(first + total[i], second)  # cur: maxAt_i = max(maxAt_[i-2] + Sums[i], maxAt_[i-1])
+            cur = max(first + total[i], second)  # cur: maxAt_i = max(maxAt_[i-2] + Sums[i-1], maxAt_[i-1])
             first = second                       # first: maxAt_(i-2)
             second = cur                         # second: maxAt_(i-1)
-        return second
+        return second                            # return nonSelect[mx+1]
 
 
 print(Solution().deleteAndEarn(nums=[2, 2, 3, 3, 3, 4]))

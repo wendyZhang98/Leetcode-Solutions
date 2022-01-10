0d849a394f8b15caf5e89280bfc71bae8afc64e3
WenxinZhang <wd1030044707@icloud.com>
2022-01-09 21:08:23 -0500

    Update and rename Best Time To Buy and Sell Stock.py to [min_price&max_profix]Best Time To Buy and Sell Stock.py

--- DynamicProgramming/[min_price&max_profix]Best Time To Buy and Sell Stock.py
+++ DynamicProgramming/[min_price&max_profix]Best Time To Buy and Sell Stock.py
@@ -22,13 +22,25 @@
 # 解释：在这种情况下, 没有交易完成, 所以最大利润为 0
 
 
+
+### Solution:
+# 在题目中，用变量 minprice 记录一个历史最低价格，假设在该历史最低点购入股票
+# 在第 i 天售出股票的利润为：prices[i]-minprice
+# 因此，我们只需遍历价格数组一遍，记录历史最低点，然后在每天思考：今天卖出能赚多少钱
+# 遍历完所有天数后，即可得到最优解
+
+
 class Solution:
     def maxProfit(self, prices):
-        res, mi = 0, prices[0]
-        for price in prices[1:]:
-            res = max(res, price - mi)
-            mi = min(mi, price)
-        return res
-
-
-print(Solution().maxProfit(prices=[7, 1, 5, 3, 6, 4]))
+        
+      # initialization
+      min_price = int(1e9)
+      max_profit = 0
+    
+      for price in prices:
+        max_profit = max(price-min_price, max_profit) # update max_profit
+        min_price = min(price, min_price)             # update min_price
+      return max_profit 
+      
+print(Solution().maxProfit(prices=[7, 1, 5, 3, 6, 4])) # 5
+print(Solution().maxProfit(prices=[7,6,4,3,1]))        # 0

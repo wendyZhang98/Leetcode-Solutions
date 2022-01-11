75ae7a7931a7ce66e904f138e6ca2382a84b1485
WenxinZhang <wd1030044707@icloud.com>
2022-01-11 18:18:46 -0500

    Update and rename Longest Common Subsequence.py to [dp[i][j]:text[0:i]和text[0:j]的最长公共子序列长度]Longest Common Subsequence.py

--- DynamicProgramming/[dp[i][j]:text[0:i]和text[0:j]的最长公共子序列长度]Longest Common Subsequence.py
+++ DynamicProgramming/[dp[i][j]:text[0:i]和text[0:j]的最长公共子序列长度]Longest Common Subsequence.py
@@ -32,6 +32,20 @@
 
 
 ### Solution:
+# 最长公共子序列问题是典型的二维动态规划问题
+# 假设字符串 text1 和 text2 的长度分别为 m 和 n
+# 创建 m+1 行 n+1 列的二维数组 dp
+# 其中 dp[i][j] 表示 text[0:i] 和 text[0:j] 的最长公共子序列长度
+
+# 考虑动态规划的边界情况 
+# 当 i=0, text[0:i] 为空，空字符串和任意字符串 最长公共子序列 长度为0
+# 因此对任意 j, dp[0][j]=0
+# 同理对任意 i, dp[i][0]=0
+
+# 当 i>0; j>0 时，考虑 dp[i][j]计算：
+# 当 text1[i-1] = text2[j-1]: dp[i][j] = dp[i-1][j-1]+1
+# 当 text1[i-1] != text2[j-1]: dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
+
 class Solution:
     def longestCommonSubsequence(self, text1, text2):
         m, n = len(text1), len(text2)
@@ -44,4 +58,4 @@ class Solution:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         return dp[-1][-1]
 
-
+print(Solution().longestCommonSubsequence(text1='ILoveYou', text2='iloveyou'))

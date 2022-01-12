e715f20ded6e5f8c9910e6ce34ab968e7c0cf864
WenxinZhang <wd1030044707@icloud.com>
2022-01-12 14:14:51 -0500

    Update and rename Matrix Block Sum.py to [sum=P[i+K+1][j+K+1]-P[i-K][j+K+1]-P[i+K+1][j-K]+P[i-K][j-K]]Matrix Block Sum.py

--- DynamicProgramming/[sum=P[i+K+1][j+K+1]-P[i-K][j+K+1]-P[i+K+1][j-K]+P[i-K][j-K]]Matrix Block Sum.py
+++ DynamicProgramming/[sum=P[i+K+1][j+K+1]-P[i-K][j+K+1]-P[i+K+1][j-K]+P[i-K][j-K]]Matrix Block Sum.py
@@ -1,14 +1,16 @@
 ### Link:
 # https://github.com/doocs/leetcode/blob/main/solution/1300-1399/1314.Matrix%20Block%20Sum/README_EN.md
+# https://leetcode-cn.com/problems/matrix-block-sum/solution/ju-zhen-qu-yu-he-by-leetcode-solution/
+
 
 
 
 ### Description:
 # 给你一个 m * n 的矩阵 mat 和一个整数 K ，请你返回一个矩阵 answer
-
 # 其中每个 answer[i][j] 是所有满足下述条件的元素 mat[r][c] 的和：
 # i - K <= r <= i + K, j - K <= c <= j + K
 # (r, c) 在矩阵内
+
 
 
 
@@ -21,6 +23,7 @@
 
 
 
+
 ### Solution:
 # 本题需要用到一些二维前缀和（prefix sum）知识
 
@@ -30,12 +33,11 @@
 
 # 题目需要对数组 mat 中每个位置
 # 计算以（i-K, j-K）为左上角，（i+K, j+K)为右下角的矩阵子数组的元素之和
-# 我们可以在前缀数组和的帮助下计算sum
+# 我们可以在前缀数组和的帮助下计算 sum
 # sum = P[i + K + 1][j + K + 1] - P[i - K][j + K + 1] - P[i + K + 1][j - K] + P[i - K][j - K]
 
 # 得到元素之和。注意到 i + K + 1、j + K - 1、i - K 和 j - K 这些下标有可能不在矩阵内
 # 因此对于所有的横坐标，我们需要将其规范在 [0, m] 的区间内
-
 # 对于所有的纵坐标，我们需要将其规范在 [0, n] 的区间内。
 
 # 具体地：
@@ -46,7 +48,6 @@
 
 # 更一般的做法是，我们对所有的横坐标与 m 取较小值，纵坐标与 n 取较小值，
 # 再将所有坐标与 0 取较大值，就可以将这些坐标规范在前缀和数组 P 的范围内。
-
 
 class Solution:
     def matrixBlockSum(self, mat, K):

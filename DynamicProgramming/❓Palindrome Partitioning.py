2c31cb280bc04005fe59c4cc4980922988d873a0
WenxinZhang <wd1030044707@icloud.com>
2022-01-14 23:32:22 -0500

    Create ❓Palindrome Partitioning.py

---
+++ DynamicProgramming/❓Palindrome Partitioning.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/palindrome-partitioning/
+
+
+
+
+### Description:
+# Given a string s, partition s such that every substring of the partition is a palindrome. 
+# Return all possible palindrome partitioning of s.
+# A palindrome string is a string that reads the same backward as forward.
+
+ 
+  
+  
+### Examples:
+# Example 1:
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+
+# Example 2:
+# Input: s = "a"
+# Output: [["a"]]
+
+
+
+
+### Solution:
+# https://leetcode.com/problems/palindrome-partitioning/solution/
+

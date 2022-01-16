7fb00dba0b764af4bd6a7351621f581b410bded4
WenxinZhang <wd1030044707@icloud.com>
2022-01-15 23:20:56 -0500

    Update ❓Palindrome Partitioning.py

--- DynamicProgramming/❓Palindrome Partitioning.py
+++ DynamicProgramming/❓Palindrome Partitioning.py
@@ -2,14 +2,12 @@
 
 
 
-
 ### Description:
 # Given a string s, partition s such that every substring of the partition is a palindrome. 
 # Return all possible palindrome partitioning of s.
 # A palindrome string is a string that reads the same backward as forward.
-
- 
   
+ 
   
 ### Examples:
 # Example 1:
@@ -22,7 +20,16 @@
 
 
 
-
 ### Solution:
-# https://leetcode.com/problems/palindrome-partitioning/solution/
+# https://leetcode.com/problems/palindrome-partitioning/discuss/1667786/Python-Simple-Recursion-oror-Detailed-Explanation-oror-Easy-to-Understand
+
+class Solution(object):
+    def partition(self, s):
+        if not s: return [[]]
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:               # prefix is a palindrome
+                for suf in self.partition(s[i:]):  # process suffix recursively
+                    ans.append([s[:i]] + suf)
+        return ans
 

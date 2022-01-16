07b3c6d53d6239cd5c07e27ca52889f033e51a11
WenxinZhang <wd1030044707@icloud.com>
2022-01-16 17:28:12 -0500

    Update Combination Sum.py

--- Search/Combination Sum.py
+++ Search/Combination Sum.py
@@ -30,3 +30,51 @@
 # Example 3:
 # Input: candidates = [2], target = 1
 # Output: []
+
+
+
+### Solutions:
+
+# This is one of the problems in the series of combination sum. 
+# They all can be solved with the same algorithm, i.e. backtracking.
+
+# Before tackling this problem, 
+# we would recommend one to start with another almost identical problem called Combination Sum III, 
+# which is arguably easier and one can tweak the solution a bit to solve this problem.
+
+# For the sake of this article, 
+# we will present the backtracking algorithm. 
+# Furthermore, we will list some other problems on LeetCode that one can solve with the same algorithm presented here.
+
+
+### BackTracking:
+
+# As a reminder, backtracking is a general algorithm for finding all (or some) solutions to some computational problems. 
+# The idea is that it incrementally builds candidates to the solutions, 
+# and abandons a candidate ("backtrack") as soon as it determines that this candidate cannot lead to a final solution.
+
+class Solution:
+    def combinationSum(self, candidates, target):
+
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
+
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
